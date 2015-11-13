3fa317679e99b589191e1fdf914b4c1490843828
Niels G. W. Serup <ngws@metanohi.name>
2015-11-13 18:04:26 +0100

    Vis kun breaking hvis det er sket indenfor de sidste to timer.

--- content/breaking_news.sh
+++ content/breaking_news.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
-import random
+import time
 import os.path
 import subprocess
 
 
-basedir = os.path.dirname(__file__)
+def show():
+    basedir = os.path.dirname(__file__)
+    breaking_file = os.path.expanduser('~/breaking_news')
+    seconds_passed = time.time() - os.stat(breaking_file).st_mtime
+    if seconds_passed > 60 * 60 * 2: # Two hours
+        return
+    
+    with open(breaking_file) as f:
+        breaking_news = f.read().strip()
+    
+    with open(os.path.join(basedir, 'res/breaking_news_skabelon.html')) as f:
+        d = f.read()
+    
+    fname = '/tmp/breaking_news.html'
+    
+    with open(fname, 'w') as f:
+        print(d.replace('{BREAKING_NEWS}', breaking_news), file=f)
+    
+    subprocess.check_call(['surf', '-p', 'file://' + fname])
 
-with open(os.path.expanduser('~/breaking_news')) as f:
-    breaking_news = f.read().strip()
-
-with open(os.path.join(basedir, 'res/breaking_news_skabelon.html')) as f:
-    d = f.read()
-
-fname = '/tmp/breaking_news.html'
-
-with open(fname, 'w') as f:
-    print(d.replace('{BREAKING_NEWS}', breaking_news), file=f)
-
-subprocess.check_call(['surf', '-p', 'file://' + fname])
+if __name__ == '__main__':
+    show()
